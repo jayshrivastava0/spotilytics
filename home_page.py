348e766d484c778390d8a862ab8ecd80f8db42b3
Jay Shrivastava <98177190+jayshrivastava0@users.noreply.github.com>
2024-03-17 23:28:07 -0400

    changes

--- home_page.py
+++ home_page.py
@@ -10,7 +10,7 @@ def home():
     st.title("Welcome to the Spotilytics")
     
     ## Header section
-    st.title("Hi, This is our Home Page for Spotilytics")
+    # st.title("Hi, This is our Home Page for Spotilytics")
     st.subheader("Spotilytics is a comprehensive database management and analytics project designed to compute essential business Key Performance Indicators (KPIs). It monitors metrics such as customer churn analysis, monthly active users (MAU), Customer Lifetime Value (CLV), and Gross Revenue, providing valuable insights for strategic decision-making and performance evaluation within the Spotify platform. Performed other analyses like Geotargeting, Follower Count, and Regional Genre Analysis to gain deeper insights into user engagement, demographic trends, and cultural preferences across different markets, ultimately enhancing content curation, personalized recommendations, and platform optimization strategies.")
 
     ## priya's container
