a75a5dbc7a6fd57d469c7a33acf6b1fc454599fb
Jay Shrivastava <98177190+jayshrivastava0@users.noreply.github.com>
2024-03-17 23:40:57 -0400

    Changes in explainitaion of insert queries

--- insert_queries.py
+++ insert_queries.py
@@ -18,6 +18,11 @@ import os
 base_path = os.getcwd()
 
 def page2():
+    st.markdown(
+        f"<h1 style='text-align: center; font-size: 70px; color: #FF0000'>Add the required fields to add record in Podcast Table</h1>",
+        unsafe_allow_html=True
+    )
+    # st.title("What is Spotilytics, you ask?")
     with open(os.path.join(base_path, "secrets.txt"), "r") as file:
         lines = file.readlines()
 
